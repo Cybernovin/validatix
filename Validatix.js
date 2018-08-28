7d973849a7ac8271597d67d1cfa73a33ac18037c
S.khansary <Siavashkhansary@gmail.com>
2018-08-28 12:32:27 +0430

    Add phoneNumber rule to rules.

--- Validatix.js
+++ Validatix.js
@@ -2,6 +2,21 @@ class validatix
 {
 	constructor()
 	{
+		this.rules = {
+			phoneNumber: function(str) {
+				let PHONENUMBER = /^(0|\+98)(9\d{2})(\d{7})$/
+				let iranPhoneCode = ["901", "902", "903", "921", "922", "990", "999"];
+				for (let i = 0; i < 10; ++i){
+					iranPhoneCode.push("91" + i.toString());
+					iranPhoneCode.push("93" + i.toString());
+				}
+				let regTest = PHONENUMBER.test(str);
+				let matches = str.match(PHONENUMBER);
+				if (typeof(matches) == undefined && matches.length == 0)
+					return false;
+				return regTest && iranPhoneCode.includes(matches[2]);
+			}
+		};
 	}
 };
 module.exports = validatix
