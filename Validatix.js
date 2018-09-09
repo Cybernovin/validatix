237f24a4318ee2e5565bb7b803d5bfd19f8b334a
S.khansary <Siavashkhansary@gmail.com>
2018-09-09 16:21:55 +0430

    Add email rule.

--- Validatix.js
+++ Validatix.js
@@ -25,6 +25,11 @@ class validatix
 		};
 
 		this.rules = {
+			email: function(data) {
+				let emailRegex = /^((?>[a-zA-Z\d!#$%&'*+\-/=?^_`{|}~]+\x20*|"((?=[\x01-\x7f])[^"\\]|\\[\x01-\x7f])*"\x20*)*(?<angle><))?((?!\.)(?>\.?[a-zA-Z\d!#$%&'*+\-/=?^_`{|}~]+)+|"((?=[\x01-\x7f])[^"\\]|\\[\x01-\x7f])*")@(((?!-)[a-zA-Z\d\-]+(?<!-)\.)+[a-zA-Z]{2,}|\[(((?(?<!\[)\.)(25[0-5]|2[0-4]\d|[01]?\d?\d)){4}|[a-zA-Z\d\-]*[a-zA-Z\d]:((?=[\x01-\x7f])[^\\\[\]]|\\[\x01-\x7f])+)\])(?(angle)>)$/i
+				return emailRegex.test(data.str);
+			},
+
 			phoneNumber: function(data) {
 				let PHONENUMBER = /^(0|\+98)(9\d{2})(\d{7})$/
 				let iranPhoneCode = ["901", "902", "903", "921", "922", "990", "999"];
